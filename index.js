95d402b7e658a3f4d107502210994d99e84e093c
phongdo240896 <doquocphong2408@gmail.com>
2025-04-04 20:31:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const puppeteer = require("puppeteer");
-const fs = require("fs");
 const axios = require("axios");
 
 (async () => {
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
+
   await page.setViewport({ width: 1920, height: 4000 });
+  await page.goto("https://iboard.ssi.com.vn", {
+    waitUntil: "networkidle2",
+    timeout: 60000,
+  });
 
-  await page.goto("https://iboard.ssi.com.vn", { waitUntil: "networkidle2", timeout: 60000 });
+  await page.waitForTimeout(8000); // đợi trang load dữ liệu
+  const screenshot = await page.screenshot({ fullPage: true });
 
-  // Đợi trang tải dữ liệu xong
-  await page.waitForTimeout(8000);
-
-  const screenshotBuffer = await page.screenshot({ fullPage: true });
-
-  await axios.post("https://tenmiencuaphong.com/upload-image.php", screenshotBuffer, {
-    headers: { "Content-Type": "application/octet-stream" }
+  // Gửi về server cPanel
+  await axios.post("https://TENMIENCUAANH.COM/upload-image.php", screenshot, {
+    headers: { "Content-Type": "application/octet-stream" },
   });
 
   await browser.close();
